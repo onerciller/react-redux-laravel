8b3e46438a3b2c5a87794f3634086212cee8f7ea
Öner Çiller <onerciller@gmail.com>
2018-08-03 18:58:23 +0300

    jwt secret has been added

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
